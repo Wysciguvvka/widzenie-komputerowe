fce1f962321717a53616e7b15890d205ff338e4d
kr33gn4r <kr33gn4r@interia.pl>
2023-12-23 15:23:24 +0100

    More structure added

---
+++ optical_flow/structure/__init__.py
@@ -0,0 +1,5 @@
+from oneway import func as onewaystructure
+from twoway import func as twowaystructure
+from pyramid import func as pyramidstructure
+
+__all__ = ["onewaystructure", "twowaystructure", "pyramidstructure"]
