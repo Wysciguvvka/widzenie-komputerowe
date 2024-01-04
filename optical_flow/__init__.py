6fa95b04fb491c52b56a060e6cf735efc0898de6
kr33gn4r <kr33gn4r@interia.pl>
2024-01-04 14:17:29 +0100

    Minor changes

--- optical_flow/__init__.py
+++ optical_flow/__init__.py
@@ -1,5 +1,5 @@
 from .gunnar_farneback import func as gunnar_farneback
 from .horn_schunck import func as horn_schunck
-from .phase_correlation import func as phase_correlation
+from .fractional_gunnar_farneback import func as fractional_gunnar_farneback
 
-__all__ = ['gunnar_farneback', 'horn_schunck', 'phase_correlation']
+__all__ = ['gunnar_farneback', 'horn_schunck', 'fractional_gunnar_farneback']
